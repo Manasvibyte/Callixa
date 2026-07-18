db4e5a1fe1ad7292db2f3882505f3836cf5033a2
Manasvi Patidar <manasvipatidar05.mp@gmail.com>
2026-07-19 02:20:18 +0530

    Fix production API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://callixa-api.onrender.com",
+  baseURL: "https://callixa-api.onrender.com/api",
 });
 
 export default api;
